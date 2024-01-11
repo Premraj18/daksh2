0b80939ada31db31e6333f4c943e9a418a7c079a
Premraj18 <premrajprasad18@gmail.com>
2024-01-12 01:41:57 +0530

    achivements

--- src/pages/About US/About.jsx
+++ src/pages/About US/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import { motion } from 'framer-motion'
-import { BallTriangle } from 'react-loader-spinner'
 
 const variants = {
     initial: {
@@ -164,16 +163,7 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <BallTriangle
-                    height={100}
-                    width={100}
-                    radius={5}
-                    color="#4fa94d"
-                    ariaLabel="ball-triangle-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    visible={true}
-                />
+                
             </Layout>
         </>
     )
